97b895ae18c3dd5ae8c4068237dcd6c446fcee0f
Alan Falloon <alan.falloon@gmail.com>
2010-02-09 21:03:50 -0500

    Make init tests more reliable and comprehensive
    Instead of checking the commit hash, this checks the hash of the
    bugs/templates tree which has the dual benefit of ensuring the bugs
    directory is put in the root, and that all the templates were created.
    
    Also, abstract out the actual correctness tests into a function so it
    can be shared among all the individual test cases.

--- test/t0000-init.sh
+++ test/t0000-init.sh
@@ -8,15 +8,45 @@ This test covers the gitbug-init commands
 
 . ./test-lib.sh
 
-test_tick
+fresh_repo () {
+    test_create_repo "$TEST_DIRECTORY/$test/t$test_count" &&
+    cd "$TEST_DIRECTORY/$test/t$test_count" &&
+    test_tick || return 1
+}
+
+init_test() {
+    gitbug init || return 1
+    git diff && git diff --cached || return 1
+    [ -z "$(git ls-files --others)" ] || return 1
+    templ_tree=$(git rev-parse --verify HEAD:bugs/templates) || return 1
+    [ "$templ_tree" == 07de27f8daf16af90f3aaa17608f9f4899280d8c ] ||
+    fail "The tree hash for bugs/templates has changed," \
+        "if you changed its contents, then this is expected, " \
+        "use 'git rev-parse HEAD:bugs/templates' to find the new hash" \
+        "And fix it in $0"
+}
 
 test_expect_success \
     'gitbug init from the root of a fresh git repo' \
-    'gitbug init &&
-     test -d bugs &&
-     git diff && git diff --cached &&
-     head=$(git rev-parse --verify HEAD) &&
-     echo $head &&
-     [ "$head" == 0b5f455ecb1873caabc1c34f6c6167a059c87474 ]'
+    'fresh_repo &&
+     init_test'
+
+test_expect_success \
+    'gitbug init from the root of a repo with changes' \
+    'fresh_repo && mkdir a b c &&
+     test_commit "add-a" a/a &&
+     test_commit "add-b" b/b &&
+     test_commit "add-c" c/c &&
+     init_test'
+
+test_expect_failure \
+    'gitbug init from a subdir of a fresh git repo' \
+    'fresh_repo && mkdir a && cd a &&
+     init_test'
+
+test_expect_failure \
+    'gitbug init from a subdir of a repo with changes' \
+    'fresh_repo && mkdir a && test_commit "add-a" a/a && cd a &&
+     init_test'
 
 test_done
